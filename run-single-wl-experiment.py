5efd027987219e2f6f3f42050149c94e546a4e41
Matthew J. Walker <mattw200@mac.com>
2017-06-19 04:59:03 +0930

    Initial commit. Basics working

---
+++ run-single-wl-experiment.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+# Matthew J. Walker
+# Created: 14 June 2017
+
+import sys
+import os
+import pandas as pd
+
+experiment_number_filename = 'experiment-number.txt'
+script_path = os.path.dirname(__file__)
+experiment_number_path = os.path.join(script_path, experiment_number_filename)
+
+def set_frequency(freq_mhz):
+    res = int(os.sysconf('SC_NPROCESSORS_ONLN'))
+    print('Number of cpus: '+str(res))
+    os.system('sudo cpufreq-set -g userspace')
+    for i in range(0, res):
+        os.system('sudo cpufreq-set -c '+str(i)+' -f '+str(freq_mhz)+'Mhz')
+
+def run_experiment(freq_mhz, core_mask, workloads_config):
+    # setup experiment directory
+    experiment_num = 0
+    try:
+        with open(experiment_number_path, 'r') as f:
+            experiment_num = int(f.read())
+        f.closed
+    except IOError:
+        pass # first experiment
+    with open(experiment_number_path, 'w') as f:
+        f.write(str(experiment_num+1))
+    f.closed
+    experiment_directory = 'powmon-experiment-{0:0>3}'.format(experiment_num)
+    if not os.path.exists(experiment_directory):
+        os.makedirs(experiment_directory)    
+    os.system('bin/pmc-setup')
+    os.system('bin/pmc-get-header > '+experiment_directory+'/pmc-events.out')
+    os.system('bin/pmc-get-header > '+experiment_directory+'/pmc-log.out')
+    set_frequency(freq_mhz)
+    # open workloads config file
+    workloads_df = pd.read_csv(workloads_config, sep='\t')
+    print workloads_df
+    # pmc setup
+    for i in range(0, len(workloads_df.index)):
+        print 'Working on: '+ workloads_df['Name'].iloc[i]
+        print 'Switching directory to: '+workloads_df['Directory'].iloc[i]
+        owd = os.getcwd()
+        try:
+            os.chdir(workloads_df['Directory'].iloc[i])
+            shell_text = '#!/usr/bin/env bash\n'
+            shell_text += ''+owd+'/bin/pmc-get-pmcs "'+workloads_df['Name'].iloc[i] \
+                    + ' start" >> '+owd+'/'+experiment_directory+'/pmc-events.out\n'
+            shell_text += 'taskset -c '+core_mask+' '+workloads_df['Command'].iloc[i] + '\n'
+            shell_text += ''+owd+'/bin/pmc-get-pmcs "'+workloads_df['Name'].iloc[i] \
+                    + ' end" >> '+owd+'/'+experiment_directory+'/pmc-events.out\n'
+            with open('temp_shell.sh', 'w') as f:
+                f.write(shell_text)
+            f.closed
+            os.system('bash temp_shell.sh')
+        finally:
+            os.chdir(owd)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--freq', dest='freq_mhz',  required=False, \
+                  help='The frequency in MHz, e.g. -f 1000')
+    parser.add_argument('-m', '--core-mask', dest='core_mask',  required=False, \
+                  help="The core mask, e.g. -m '0,1,2,3'")
+    parser.add_argument('-c', '--workloads-config', dest='workloads_config', required=True, \
+                  help="The workload config file, e.g. -c 'workloads.config'")
+    args=parser.parse_args()
+    freq = 1000
+    if args.freq_mhz:
+        freq = args.freq_mhz
+    core_mask = '0,1,2,3'
+    if args.core_mask:
+        core_mask = args.core_mask
+    run_experiment(freq, core_mask, args.workloads_config)
