278d4303802d620a3bec51506874892cf8a4a5c1
Matthew J. Walker <mattw200@mac.com>
2017-07-18 00:25:02 +0930

    Analysis tested, basics working

--- run_experiment.py
+++ run_experiment.py
@@ -15,6 +15,26 @@ experiment_number_path = os.path.join(script_path, experiment_number_filename)
 FILENAME_PMC_EVENTS_LOG = 'pmc-events-log.out'
 FILENAME_PMC_CONTINUOUS_LOG = 'pmc-continuous-log.out'
 
+cpu_ids = {
+    'Cortex-A8' : '0x00',
+    'Cortex-A7' : '0x07',
+    'Cortex-A15' : '0x0F',
+    'Cortex-A53' : '0x03',
+    'Cortex-A57' : '0x01',
+    'Cortex-A72' : '0x02', 
+    'Cortex-A73' : '0x04'
+    }
+
+cpu_num_counters = {
+    'Cortex-A8' : 4,
+    'Cortex-A7' : 4,
+    'Cortex-A15' : 6,
+    'Cortex-A53' : 6,
+    'Cortex-A57' : 6,
+    'Cortex-A72' : 6, 
+    'Cortex-A73' : 6
+    }
+
 #sudo ./bin/pmc-run 200000 > temp
 
 class ContinuousLogging(threading.Thread): 
@@ -39,6 +59,8 @@ def set_frequency(freq_mhz):
         os.system('sudo cpufreq-set -c '+str(i)+' -f '+str(freq_mhz)+'Mhz')
 
 def run_experiment(freq_mhz, core_mask, workloads_config):
+    import time
+    import datetime
     # setup experiment directory
     experiment_num = 0
     try:
@@ -82,19 +104,37 @@ def run_experiment(freq_mhz, core_mask, workloads_config):
             os.system('bash temp_shell.sh')
         finally:
             os.chdir(owd)
+    print ("Finished at: "+str(datetime.datetime.now())+" (" \
+            +str(int(round(time.time() * 1000)))+")")
     print ("Waiting for logging thread to finish")
     os.system("echo '0' | sudo tee PMC_RUN_CHECK") # Stop data logging
     loggingThread.join()
+    print ("Exiting at: "+str(datetime.datetime.now())+" (" \
+            +str(int(round(time.time() * 1000)))+")")
+
+#def parse_pmcs_string(pmcs_string_dict):
+    # pmc_strings string is an array of strings
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--freq', dest='freq_mhz',  required=False, \
                   help='The frequency in MHz, e.g. -f 1000')
-    parser.add_argument('-m', '--core-mask', dest='core_mask',  required=False, \
+    parser.add_argument('-m', '--core-mask', dest='core_mask', required=False, \
                   help="The core mask, e.g. -m '0,1,2,3'")
-    parser.add_argument('-c', '--workloads-config', dest='workloads_config', required=True, \
+    parser.add_argument('-c', '--workloads-config', dest='workloads_config', \
+                  required=True, \
                   help="The workload config file, e.g. -c 'workloads.config'")
+    parser.add_argument('--pmcs-file', dest='pmcs_file', required=False, \
+                  help="Specifies the pmcs-setup.txt file to use.")
+    '''
+    parser.add_argument('--pmcs-a7', dest='pmcs_a7', required=False \
+                  help="Set PMCs for Cortex-A7. E.g. "
+                  +'--pmcs-a7 "0x1b,0x50,0x6A,0x73"'
+    parser.add_argument('--pmcs-a15', dest='pmcs_a15', required=False \
+                  help="Set PMCs for Cortex-A15. E.g. "
+                  +'--pmcs-a15 "0x1b,0x50,0x6A,0x73,0x14,0x19"'
+    '''
     args=parser.parse_args()
     freq = 1000
     if args.freq_mhz:
@@ -102,4 +142,9 @@ if __name__ == "__main__":
     core_mask = '0,1,2,3'
     if args.core_mask:
         core_mask = args.core_mask
+    # TODO
+    '''
+    if args.pmcs_file:
+    '''
+
     run_experiment(freq, core_mask, args.workloads_config)
